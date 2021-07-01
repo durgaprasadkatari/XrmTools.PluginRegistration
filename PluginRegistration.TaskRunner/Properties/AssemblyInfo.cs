a4dd964d5815b202c2e6a52cc84ed68eef0546b9
Durgaprasad Katari <durgaprasadkatari@gmail.com>
2021-07-01 09:14:16 +0100

    Updated assembly version

--- PluginRegistration.TaskRunner/Properties/AssemblyInfo.cs
+++ PluginRegistration.TaskRunner/Properties/AssemblyInfo.cs
@@ -1,17 +1,18 @@
-﻿using System.Reflection;
+﻿using System.Globalization;
+using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("PluginRegistration.Deployment")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("PluginRegistration.TaskRunner")]
+[assembly: AssemblyDescription("Simple and Lightweight plugins deployment task runner for Dynamics 365")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("PluginRegistration.Deployment")]
+[assembly: AssemblyCompany("Durgaprasad Katari")]
+[assembly: AssemblyProduct("PluginRegistration.TaskRunner")]
 [assembly: AssemblyCopyright("Copyright ©  2021")]
-[assembly: AssemblyTrademark("")]
+[assembly: AssemblyTrademark("Durgaprasad")]
 [assembly: AssemblyCulture("")]
 
 // Setting ComVisible to false makes the types in this assembly not visible
@@ -32,5 +33,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.1.0")]
+[assembly: AssemblyFileVersion("1.0.1.0")]
